17ac5eda80eb3a7f9cb7dda941734792fa4d725b
Feliks <137520426+FelixUXUI@users.noreply.github.com>
2023-12-16 17:06:15 +0600

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,97 +1,39 @@
-//TAB SLIDER
-const tabContendBlocks = document.querySelectorAll('.tab_content_block')
-const tabsParentBlock = document.querySelector('.tab_content_items')
-const tabsBlocks = document.querySelectorAll('.tab_content_item')
+const { modal, modalTrigger, modalCloseButton } = document.querySelectorAll('.modal, #btn-get, .modal_close');
 
-
-
-const hideTabContend = () => {
-    tabContendBlocks.forEach(tabContendBlock=> {
-        tabContendBlock.style.display = 'none'
-    })
-    tabsBlocks.forEach(tabBlock => {
-        tabBlock.classList.remove('tab_content_item_active')
-
-    })
-}
-const showTabContends = (indexElement = 0) => {
-    tabContendBlocks[indexElement].style.display = 'block'
-    tabsBlocks[indexElement].classList.add('tab_content_item_active')
-}
-hideTabContend()
-showTabContends()
-
-tabsParentBlock.onclick = (event) => {
-    if (event.target.classList.contains('tab_content_item')){
-        tabsBlocks.forEach((tabBlock, tabIndex)=> {
-            if (event.target === tabBlock){
-                hideTabContend()
-                showTabContends(tabIndex)
-            }
-        })
-    }
-}
-let sliderIndex = 0
-const autoSlider = () => {
-    hideTabContend()
-    sliderIndex = (sliderIndex + 1) % tabContendBlocks.length
-    showTabContends(sliderIndex)
-}
-setInterval(autoSlider, 3000)
-
-
-// //MODAL
-const modal = document.querySelector('.modal')
-const modalTrigger = document.querySelector('#btn-get')
-const modalCloseButton = document.querySelector('.modal_close')
 const openModal = () => {
-    modal.style.display = 'block'
-    document.body.style.overflow = 'hidden'
-    modalWasShown = true
-
-}
+  modal.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+  modalWasShown = true;
+};
 
 const closeModal = () => {
-    modal.style.display = 'none'
-    document.body.style.overflow = ''
+  modal.style.display = 'none';
+  document.body.style.overflow = '';
+};
 
-}
-modalTrigger.onclick = () => { openModal() }
+modalTrigger.addEventListener('click', openModal);
+modalCloseButton.addEventListener('click', closeModal);
 
-modalCloseButton.onclick = () => { closeModal() }
+modal.addEventListener('click', e => {
+  if (e.target === modal) closeModal();
+});
 
-modal.onclick = () => {
-    if (event.target === modal) closeModal()
-}
+let modalWasShown = false;
 
-
-let modalWasShown = false
 window.addEventListener('scroll', () => {
-    const scrollHeight = window.innerHeight + window.scrollY;
-    const pageHeight = document.documentElement.scrollHeight;
+  const scrollHeight = window.innerHeight + window.scrollY;
+  const pageHeight = document.documentElement.scrollHeight;
 
-    if (scrollHeight >= pageHeight * 1 && !modalWasShown) {   // ЗАПОМНИТЬ ЧТО НЕ РАВНО В УСЛОВИЯХ ОБОЗНАЧАЕТСЯ ТАК
-        openModal()
+  if (scrollHeight >= pageHeight && !modalWasShown) {
+    openModal();
+  }
+});
 
-    }
+let showModal = false;
 
-})
-
-let showModal = false
-const timeOut = () => {
-    if (!showModal) {
-        setTimeout(openModal, 10000)
-        showModal = true
-    }
-}
-timeOut()
-
-
-
-
-
-
-
-
-
-
+setTimeout(() => {
+  if (!showModal) {
+    openModal();
+    showModal = true;
+  }
+}, 10000);
